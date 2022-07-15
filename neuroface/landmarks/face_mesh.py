3cfc10314079a7edb4b7404a3f004815b6b72c95
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-07-15 15:29:07 +0300

    Updated MediaPipe Face Mesh

--- neuroface/landmarks/face_mesh.py
+++ neuroface/landmarks/face_mesh.py
@@ -3,26 +3,40 @@ import numpy as np
 import mediapipe
 
 
-class MediapipeMesh(object):
-    """ MediaPipe Face Mesh implementation for face mesh detection.
+class FaceMesh(object):
+    """ MediaPipe Face Mesh implementation.
     
-    Args:
-        image (np.ndarray): BGR image represented as numpy ndarray.
-        model (module): MediaPipe solution Python API.
-        static_image_mode (int): Whether to treat the input images as a batch
-            of static and possibly unrelated images, or a video stream.
-        max_num_faces (int): Maximum number of faces to detect.
-            refine_landmarks (bool): Whether to further refine the landmark coordinates
-            around the eyes and lips, and output additional landmarks around the irises.
-        min_detection_confidence (float): Minimum confidence value ([0.0, 1.0]) for face
-            detection to be considered successful.
-        min_tracking_confidence (float): Minimum confidence value ([0.0, 1.0]) for the
-            face landmarks to be considered tracked successfully.
-        normalize (bool): Apply minimax normalization to facial landmark.
+    MediaPipe Face Mesh processes an RGB image and returns 478 face landmarks on each detected face.
+    
+    Example:
+        >>> import cv2
+        >>> from neuroface import FaceMesh
+        >>> face_mesh = FaceMesh(static_image_mode=True, max_num_faces=1)
+        >>> image = cv2.imread('test.jpg')
+        >>> face_array = face_mesh.detect(image)
     """
     
-    def __init__(self, static_image_mode=False, max_num_faces=1, refine_landmarks=True,
-                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
+    def __init__(
+        self,
+        static_image_mode: bool = False,
+        max_num_faces: int = 1,
+        refine_landmarks: bool = True,
+        min_detection_confidence: float = 0.5,
+        min_tracking_confidence: float = 0.5
+    ) -> None:
+        """
+        Args:
+            static_image_mode (int, optional): Whether to treat the input images as a batch
+                of static and possibly unrelated images, or a video stream.
+            max_num_faces (int, optional): Maximum number of faces to detect.
+            refine_landmarks (bool, optional): Whether to further refine the landmark coordinates
+                around the eyes and lips, and output additional landmarks around the irises.
+            min_detection_confidence (float, optional): Minimum confidence value ([0.0, 1.0]) for face
+                detection to be considered successful.
+            min_tracking_confidence (float, optional): Minimum confidence value ([0.0, 1.0]) for the
+                face landmarks to be considered tracked successfully.
+        """
+        
         super().__init__()
         
         mesh_detector = mediapipe.solutions.face_mesh
@@ -30,21 +44,30 @@ class MediapipeMesh(object):
             max_num_faces=max_num_faces, refine_landmarks=refine_landmarks,
             min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
     
-    def detect(self, image, normalize=None):
+    def detect(self, image: np.ndarray, normalize: bool = False) -> np.ndarray:
+        """
+        Args:
+            image (np.ndarray): BGR image represented as numpy ndarray.
+            normalize (bool, optional)): Apply minimax normalization to facial landmarks.
+        """
+        
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
-        face_vector = np.array([])
+        face_array = []
         prediction = self.model.process(image)
         
         if prediction.multi_face_landmarks:
-            landmarks = prediction.multi_face_landmarks[0].landmark
-            face_vector = np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks])
-            
-            if normalize:
-                # Normalization and centering of the face from -0.5 to 0.5 in width, height and depth of the face.
-                min_value = face_vector.min(axis=0)
-                max_value = face_vector.max(axis=0)
+            for idx in range(len(prediction.multi_face_landmarks)):
+                landmarks = prediction.multi_face_landmarks[idx].landmark
+                face_vector = np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks])
                 
-                face_vector = np.absolute(face_vector - min_value) / np.absolute(max_value - min_value) - 0.5
+                if normalize:
+                    # Normalization and centering of the face from -0.5 to 0.5 in width, height and depth of the face.
+                    min_value = face_vector.min(axis=0)
+                    max_value = face_vector.max(axis=0)
+                    
+                    face_vector = np.absolute(face_vector - min_value) / np.absolute(max_value - min_value) - 0.5
+                
+                face_array.append(face_vector)
         
-        return face_vector
+        return np.array(face_array)
