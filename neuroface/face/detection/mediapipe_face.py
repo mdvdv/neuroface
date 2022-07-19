ad175ab5faec1ae872a7b2909b65f3172d88ff8a
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-07-19 12:18:10 +0300

    Updated FaceDetection

--- neuroface/face/detection/mediapipe_face.py
+++ neuroface/face/detection/mediapipe_face.py
@@ -1,54 +1,79 @@
 import cv2
 import numpy as np
 import mediapipe
+from typing import List
 
 
-class MediapipeFace(object):
-    """ MediaPipe model implementation for face detection.
+class FaceDetection(object):
+    """ MediaPipe Face Detection implementation.
     
-    Args:
-        image (np.ndarray): BGR image represented as numpy ndarray.
-        model (module): MediaPipe solution Python API.
-        model_selection (int): 0 or 1. 0 to select a short-range model that works
-            best for faces within 2 meters from the camera, and 1 for a full-range
-            model best for faces within 5 meters.
-        min_detection_confidence (float): Minimum confidence value ([0.0, 1.0]) for face
-            detection to be considered successful.
+    MediaPipe Face Detection processes an RGB image and returns a list of the detected faces.
+    
+    Example:
+        >>> import cv2
+        >>> from neuroface import FaceDetection
+        >>> face_detector = FaceDetection(model_selection=0, min_detection_confidence=0.9)
+        >>> image = cv2.imread('test.jpg')
+        >>> face_array = face_mesh.extract(image)
     """
     
-    def __init__(self, model_selection=0, min_detection_confidence=0.5):
+    def __init__(
+        self,
+        model_selection: int = 0,
+        min_detection_confidence: float = 0.5
+    ) -> None:
+        """
+        Args:
+            model_selection (int, optional): 0 or 1. 0 to select a short-range model that works
+                best for faces within 2 meters from the camera, and 1 for a full-range
+                model best for faces within 5 meters.
+            min_detection_confidence (float, optional): Minimum confidence value ([0.0, 1.0]) for face
+                detection to be considered successful.
+        """
+        
         super().__init__()
         
         face_detector = mediapipe.solutions.face_detection
         self.model = face_detector.FaceDetection(model_selection=model_selection,
             min_detection_confidence=min_detection_confidence)
     
-    def detect(self, image):
+    def detect(self, image: np.ndarray) -> List:
+        """
+        Args:
+            image (np.ndarray): BGR image represented as numpy ndarray.
+        """
+        
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_height, image_width = image.shape[:2]
         
-        face_location = np.array([])
+        face_array = []
         prediction = self.model.process(image)
         
         if prediction.detections:
-            bounding_box = prediction.detections[0].location_data.relative_bounding_box
-            face_location = np.array([[int(bounding_box.xmin * image_width), int(bounding_box.ymin * image_height)],
-                                      [int(bounding_box.width * image_width), int(bounding_box.height * image_height)]])
+            for idx in range(len(prediction.detections)):
+                bounding_box = prediction.detections[idx].location_data.relative_bounding_box
+                face_location = np.array([[int(bounding_box.xmin * image_width), int(bounding_box.ymin * image_height)],
+                                          [int(bounding_box.width * image_width), int(bounding_box.height * image_height)]])
+                
+                face_array.append(face_location)
         
-        return face_location
+        return np.array(face_array)
     
-    def extract(self, image):
+    def extract(self, image: np.ndarray) -> np.ndarray:
         """ Cropping area where the face is located.
         
         Args:
             image (np.ndarray): BGR image represented as numpy ndarray.
         """
         
-        face_image = np.array([])
-        face_location = self.detect(image=image)
+        face_batch = []
+        face_array = self.detect(image=image)
         
-        if face_location is not None:
-            face_image = image[face_location[0][1]:face_location[0][1]+face_location[1][1],
-                               face_location[0][0]:face_location[0][0]+face_location[1][0]]
+        if face_array is not None:
+            for face_location in face_array:
+                face_image = image[face_location[0][1]:face_location[0][1]+face_location[1][1],
+                                   face_location[0][0]:face_location[0][0]+face_location[1][0]]
+                
+                face_batch.append(face_image)
         
-        return face_image
+        return face_batch
