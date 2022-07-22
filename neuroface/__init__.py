9efcd35bd7a8d20852205e547a57ad5c87bb5f1a
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-07-22 13:21:29 +0300

    Update __init__.py

--- neuroface/__init__.py
+++ neuroface/__init__.py
@@ -1,5 +1,6 @@
 from .face.detection.mtcnn import MTCNN
 from .face.detection.mediapipe_face import FaceDetection
+from .face.detection.retinaface import RetinaFace
 from .face.comparison.inception_resnet_v1 import InceptionResnetV1
 from .landmarks.face_mesh import FaceMesh
 from .emotions.attention_resnet import AttentionResnet
