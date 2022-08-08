9c84297861b54b658412d1c732be28f1279d6459
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-08-08 14:03:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
       'torchvision>=0.13.0',
       'mediapipe>=0.8.10.1',
       'opencv_python>=4.6.0.66',
-      'numpy>=1.23.1',
       'Pillow>=9.2.0',
       'av>=9.2.0',
       'gdown>=4.5.1'
