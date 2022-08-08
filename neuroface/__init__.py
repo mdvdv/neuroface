cdb8069d834e45a749e62d3b205318f562f1f1f2
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-08-08 14:12:15 +0300

    Update __init__.py

--- neuroface/__init__.py
+++ neuroface/__init__.py
@@ -1,5 +1,3 @@
-__import__('pkg_resources').declare_namespace(__name__)
-
 from .face.detection.mtcnn import MTCNN
 from .face.detection.face_detection import FaceDetection
 from .face.detection.retinaface import RetinaFace
