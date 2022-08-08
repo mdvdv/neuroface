8b0b4c704bf9b56faf5af51550af4a31eaef51e4
Anatoly Medvedev <83948828+mdvdv@users.noreply.github.com>
2022-08-08 14:15:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
+NAME = 'neuroface'
+VERSION = '0.1.0'
+DESCRIPTION = 'Human face detection, recognition on video.'
+URL = 'https://github.com/mdvdv/neuroface'
+
 setup(
-    name='neuroface',
-    version='0.1.0',
-    url='https://github.com/mdvdv/neuroface',
-    description='Human face detection, recognition on video.',
-    packages=find_packages(),    
+    name=NAME,
+    version=VERSION,
+    url=URL
+    description=DESCRIPTION,
+    packages={
+        'neuroface',
+        'neuroface.emotions',
+        'neuroface.face',
+        'neuroface.face.comparison',
+        'neuroface.face.detection',
+        'neuroface.landmarks',
+    ],
     install_requires=[
       'torch>=1.12.0',
       'torchvision>=0.13.0',
